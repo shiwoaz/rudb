34bd5c639ca25d7c2c0bcd98e74533566dc3d264
ismeovo <1219353840@qq.com>
2023-02-22 08:23:57 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
 mod common;
-use common::print_prompt;
+use common::{commands, print_prompt};
 
 mod input_buffer;
 use input_buffer::InputBuffer;
-use std::{io, process::exit};
-
-pub fn commands(buffer: &InputBuffer) {
-    let s = &buffer.buffer.trim()[..]; // remove new line char
-    match s {
-        _ if s == ".exit" => exit(0),
-        un => println!("{} unrecognized command!", un),
-    };
-}
 
 fn main() {
     let mut input_buffer = InputBuffer::new();
